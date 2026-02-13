349036da4b5efe498e073748c49a55ec95ff8be6
Amar <skamar003@gmail.com>
2026-02-13 15:23:49 +0530

    update my arr1

--- Java_Basices/src/JavaFrisher_coading/RemoveDuplicatesGivenArray.java
+++ Java_Basices/src/JavaFrisher_coading/RemoveDuplicatesGivenArray.java
@@ -7,6 +7,8 @@ public class RemoveDuplicatesGivenArray {
 	public static void main(String[] args) {
 		
 		int[] arr = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5};
+		int[] arr1 = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5};
+
 		int visited=-1;
 		for(int i=0;i<arr.length;i++) {
 			if(arr[i]!= visited) {
